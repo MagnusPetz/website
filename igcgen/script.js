489faefd8007cfe6808843cb89a7f96f8875de0e
UltraDevMC <petzmagnus@gmail.com>
2019-10-20 17:30:31 +0200

    Created distance function (for latitude and longitude)

--- igcgen/script.js
+++ igcgen/script.js
@@ -232,6 +232,21 @@ function touchMoved() {
 	ellipse(mouseX, mouseY, 2, 2);
 }
 
+function distance(lat1, lon2, lat2, lon2) {
+	var R = 6371e3;
+	var φ1 = lat1.toRadians();
+	var φ2 = lat2.toRadians();
+	var Δφ = (lat2-lat1).toRadians();
+	var Δλ = (lon2-lon1).toRadians();
+
+	var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+	var d = R * c;
+}
+
 class BRecord {
   constructor(hr, mn, sec, lat, lon) {
     this.hr = hr;
